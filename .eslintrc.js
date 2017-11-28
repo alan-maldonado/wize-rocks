41dda38ae4ae0e77f775df3dbc573f65ea65f22f
alan-maldonado <alan.maldonado@wizeline.com>
2017-11-28 11:40:57 -0600

    :rotating_light: Fixing linter configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,10 +14,6 @@ module.exports = {
   env: {
     browser: true,
   },
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
   // add your custom rules here
   'rules': {
     // allow debugger during development
